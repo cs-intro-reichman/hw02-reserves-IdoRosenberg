3f610deb24a526ed7b8eb24ea2b2f411a29c9dff
Ido Rosenberg <ido.rosenberg@post.runi.ac.il>
2024-01-27 17:21:36 +0200

    Changes to OneOfEachStats

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -1,4 +1,4 @@
-import java.util.Random;
+	import java.util.Random;
 /**
  *  Computes some statistics about families in which the parents decide 
  *  to have children until they have at least one child of each gender.
@@ -24,6 +24,74 @@ public class OneOfEachStats {
 		//// just like you had in the previous version, except that the 
 		//// randomization will be based on the given seed.
 		//// This is the only change that you have to do in the program.
+		
+		int repeat = Integer.parseInt(args[0]);
+		double average = 0.0;
+		int famOfTwo = 0;
+		int famOfThree = 0;
+		int famOfFour = 0;
+		
+		for (int i=0; i<repeat; i++){
+		int amountOfChilds = 0;
+		boolean boy = true;
+		boolean notSame = true;
+		
+		double gen1 = Math.random();
+			if (gen1 > 0.5){	
+				amountOfChilds++;
+				while(notSame){
+					double gen2 = Math.random();
+					if (gen2 > 0.5){
+						amountOfChilds++;
+					}
+					else{
+						amountOfChilds++;
+						notSame = false;
+					}
+				}
+			}
+			else {
+				amountOfChilds++;
+				while(notSame){
+					double gen2 = Math.random();
+					if (gen2 < 0.5){
+						amountOfChilds++;
+					}
+					else{
+						amountOfChilds++;
+						notSame = false;
+					}
+				}
+			}
+
+			average += amountOfChilds;
+			
+			switch(amountOfChilds){
+				case 2:
+				famOfTwo++;
+				break;
+				case 3:
+				famOfThree++;
+				break;
+				case 4:
+				famOfFour++;
+				break;
+				default:
+				famOfFour++;
+			}	
+	}
+	
+		System.out.println("Average: " + average/repeat + " children to get at least one of each gender.");
+		System.out.println("Number of families with 2 children: "+famOfTwo);
+		System.out.println("Number of families with 3 children: "+famOfThree);
+		System.out.println("Number of families with 4 or more children: "+famOfFour);
+		
+		if (famOfTwo >= famOfThree && famOfTwo >= famOfFour)
+			System.out.println("The most common number of children is 2.");
+		else if(famOfThree >= famOfFour)
+			System.out.println("The most common number of children is 3.");
+		else
+			System.out.println("The most common number of children is 4 or more.");
 		    
 	}
 }
