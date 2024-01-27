821962955c34487835120cb6934f1a59409cdad6
Ido Rosenberg <ido.rosenberg@post.runi.ac.il>
2024-01-27 17:06:23 +0200

    Changes to Damka

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -4,13 +4,13 @@
 public class DamkaBoard {
 	public static void main(String[] args) {
 		int size = Integer.parseInt(args[0]);
-		int i = 0;
-		int j = 0;
+		int i = 1;
+		int j = 1;
 		boolean needSpace = true;
 		
-		while(i < size){
-			while (j < size){
-				if(j == (size-1))
+		while(i <= size){
+			while (j <= size){
+				if(j == size)
 					System.out.print("*");
 				else
 					System.out.print("* ");
@@ -19,7 +19,7 @@ public class DamkaBoard {
 			}
 			System.out.println();
 			i++;
-			j = 0;
+			j = 1;
 			if (needSpace){
 				System.out.print(" ");
 				needSpace = false;
